5c07b8cfacca3390561397509bbe9aa31943cb27
sabinosabi <sabinosabi@users.noreply.github.com>
2018-12-13 23:18:35 +0900

    役割分担表からとりあえず適当に関数わけした
    関数は個人で増やしても構いません。　構造体とかいると思うからそれは全体で決めるべきかな

---
+++ main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+FILE FileRead(char[] filepath){
+    return;
+}
+
+void Output(){
+    return;
+}
+
+void CheckAnswer(){
+    return;
+}
+
+void Menu(){
+    return;
+}
+
+void ManageTag(){
+    return;
+}
+
+void CreateProblem(){
+    return;
+}
+
+int main(){
+
+    return 0;
+}
